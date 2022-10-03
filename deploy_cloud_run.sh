1d02acc1668b91b83b474a3a20b79eea934d5608
quangdat <doquangdat_t59@gmail.com>
2022-10-03 22:44:59 +0700

    update deploy script

--- deploy_cloud_run.sh
+++ deploy_cloud_run.sh
@@ -1,24 +1,4 @@
-gcloud secrets add-iam-policy-binding DATABASE_NAME --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding USER_SECRET --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding DEVICE_SECRET --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding USER_TABLE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding REGISTER_TABLE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding FIREBASE_SECRET --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding AVATAR_BUCKET --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding OTP_SENDER --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding OTP_KEYWORD --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding OTP_MESSAGE_TEMPLATE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding OTP_SECRET_KEY --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding OTP_PARTNER_CODE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding OTP_LOGGING_TABLE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding AVARTAR_COVI_LIFE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding MONGO_SERVER --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding USER_AVATAR_BUCKET --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding USER_AVARTAR_COVI_LIFE --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud secrets add-iam-policy-binding NUMBER_PATIENT_LIMITED --role roles/secretmanager.secretAccessor --member serviceAccount:693115483107-compute@developer.gserviceaccount.com
-gcloud projects add-iam-policy-binding glassy-totality-324307 --member serviceAccount:693115483107-compute@developer.gserviceaccount.com --role roles/artifactregistry.reader
-
 docker build --build-arg EXTRA_REPO_URL=https://oauth2accesstoken:$(gcloud auth print-access-token)@asia-south1-python.pkg.dev/glassy-totality-324307/covi-life-repository/simple . -t=globicare-realtime:latest --no-cache
-docker tag globicare -realtime gcr.io/glassy-totality-324307/globicare-realtime
+docker tag globicare-realtime gcr.io/glassy-totality-324307/globicare-realtime
 gcloud docker -- push gcr.io/glassy-totality-324307/globicare-realtime
 gcloud run deploy globicare-realtime --image gcr.io/glassy-totality-324307/globicare-realtime --region=asia-south1 --allow-unauthenticated --vpc-connector=covi-life-mongo-connector --vpc-egress=all-traffic
